2320f46d4cd23a2a6f04299254e181b95fa6a56e
vierge <marancholia@gmail.com>
2020-01-15 12:36:58 -0500

    Achieved MOVEMENT

--- client.js
+++ client.js
@@ -5,15 +5,22 @@ const connect = function(ip) {
     host: ip,
     port: 50541
   });
+  conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('connection 2 snek established!');
     conn.write('Name: MRG');
+    
+    setInterval(() => {
+      conn.write('Move: up');
+    }, 100);
+
   });
   conn.setEncoding('utf8');
 
   conn.on('data', (incData) => {
     console.log(incData);
   });
+
 
   return conn;
 }
