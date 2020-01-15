aa58ee52f63366d7a67baeafa57e3e207526a4ea
vierge <marancholia@gmail.com>
2020-01-15 13:01:33 -0500

    Refactored (modularized) input setup and handler

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (data) => {
-    handleUserInput(data);
-  });
-  return stdin;
-}
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
 console.log('connecting....');
 connect('172.46.2.204');
